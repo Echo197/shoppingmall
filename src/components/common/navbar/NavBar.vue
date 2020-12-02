9c034f2bbb71f3ca27b92d5dfbb31e283b9d6469
Olivia <15183315062@163.com>
2020-12-02 19:32:13 +0800

    导航组件+首页导航样式修改

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navbar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style>
+.navbar{
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0 2px 2px rgba(100,100,100,.2);
+}
+.left,.right{
+    width: 66px;
+}
+.center{
+    flex: 1;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>3</div>
+  <nav-bar class="homeNav">
+    <div slot="center">购物街</div>
+  </nav-bar>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
 export default {
   data() {
     return {};
+  },
+  components: {
+    NavBar,
   },
   methods: {},
 };
 </script>
 <style>
+.homeNav {
+  color: var(--color-background);
+  background-color: var(--color-tint);
+}
 </style>
