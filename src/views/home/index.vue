003599d404f40962adb5f37a6593a238154ade8e
Olivia <15183315062@163.com>
2020-12-02 20:35:04 +0800

    网络请求request.js+home.js

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,12 +6,27 @@
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import { getMultiData } from "network/api/home";
 export default {
   data() {
-    return {};
+    
+    return {
+      banner: "",
+      dKeyword: "",
+      keywords: "",
+      recommend: "",
+    };
   },
   components: {
     NavBar,
+  },
+  created() {
+    getMultiData().then((res) => {
+      this.banner = res.banner;
+      this.dKeyword = res.dKeyword;
+      this.keywords = res.keywords;
+      this.recommend = res.recommend;
+    });
   },
   methods: {},
 };
