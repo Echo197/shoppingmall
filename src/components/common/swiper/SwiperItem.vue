b9b14333ff86351ba489dcf9a1a9a32abdec95e2
Olivia <15183315062@163.com>
2020-12-08 21:13:35 +0800

    轮播图

---
+++ src/components/common/swiper/SwiperItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="slide">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwiperItem",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style>
+.slide {
+  width: 100%;
+  /* 若是父元素设置了flex布局后，若要其子元素的width有效果，必须给子元素设置flex-shrink为0 */
+  flex-shrink: 0;
+  height: 150px;
+  background-color: pink;
+}
+
+.slide img {
+  width: 100%;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,37 +1,60 @@
 <template>
-  <nav-bar class="homeNav">
-    <div slot="center">购物街</div>
-  </nav-bar>
+  <div id="home">
+    <!-- 导航栏 -->
+    <nav-bar class="homeNav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <!-- 轮播图 -->
+    <!-- 思路一 -->
+    <!-- <swiper :banners="banner"></swiper> -->
+    <!-- 思路二 -->
+    <swiper>
+      <swiper-item v-for="(item, index) in banner" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import Swiper from "components/common/swiper/Swiper";
+import SwiperItem from "components/common/swiper/SwiperItem";
+// import { Swiper, SwiperItem } from "components/common/swiper";
 import { getMultiData } from "network/api/home";
 export default {
+  name: "home",
+  components: {
+    NavBar,
+    Swiper,
+    SwiperItem,
+  },
   data() {
-    
     return {
-      banner: "",
+      banner: [],
       dKeyword: "",
       keywords: "",
       recommend: "",
     };
   },
-  components: {
-    NavBar,
-  },
   created() {
     getMultiData().then((res) => {
-      this.banner = res.banner;
-      this.dKeyword = res.dKeyword;
-      this.keywords = res.keywords;
-      this.recommend = res.recommend;
+      this.banner = res.data.banner.list;
+      console.log(this.banner);
+      this.dKeyword = res.data.dKeyword;
+      this.keywords = res.data.keywords;
+      this.recommend = res.data.recommend;
     });
   },
   methods: {},
 };
 </script>
 <style>
+#home {
+  height: 100vh;
+}
 .homeNav {
   color: var(--color-background);
   background-color: var(--color-tint);
